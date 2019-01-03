d7869bdf8eb89cbca3060b2cde38c245d18cd047
taoqy <wy3502192282@163.com>
2019-01-03 14:56:32 +0800

    令牌桶和漏桶

--- Zuul/src/main/java/com/taoqy/ZuulApplication.java
+++ Zuul/src/main/java/com/taoqy/ZuulApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulServer;
  * zuul-ratelimit 原理是利用了zuulfilter，通过创建pre zuulfilter 和post zuulfilter来实现各种策略
  * 使用者可以通过自定义DefaultRateLimitKeyGenerator 里的key方法来确定请求者的唯一性
  * 通过配置repository 来存储请求者的请求次数，响应时间以及刷新时间。
- *
+ * 这种算法更像“令牌桶算法” 而非 “漏桶算法”
  * @author Taoqy
  * @version 1.0, 2018/12/24
  * @see [相关类/方法]
